655de6752ec117f7a452dcb8f91ccd200b350e3f
Bianca Dias <bianca.dias@ufv.br>
2023-09-03 14:23:01 -0300

    fix: cryptr in service

--- src/credentials/credentials.service.ts
+++ src/credentials/credentials.service.ts
@@ -7,39 +7,44 @@ import Cryptr from 'cryptr';
 
 @Injectable()
 export class CredentialsService {
- 
-  constructor(private readonly repository: CredentialRepository){}
+  private cryptr: Cryptr
+  constructor(private readonly repository: CredentialRepository){
+    const Cryptr = require('cryptr');
+    this.cryptr = new Cryptr(process.env.SECRET_CRYPTR);
+  }
 
   async create(createCredentialDto: CreateCredentialDto, user: Users) {
-    // const cryptr = new Cryptr("MyKey")
-    // const hash = cryptr.encrypt(createCredentialDto.password)
-    // createCredentialDto.password = hash;
+    const hash = this.cryptr.encrypt(createCredentialDto.password)
+    createCredentialDto.password = hash;
     return await this.repository.create(createCredentialDto, user.id);
   }
 
   async findAll(user: Users) {
-    // const cryptr = new Cryptr("MyKey")
-    return await this.repository.findAll(user.id);
-    // const credentialsDescrypt = credentials.map((credential)=>({
-    //   ...credential,
-    //   password: cryptr.decrypt(credential.password)
-    // }))
-    // return credentialsDescrypt;
+    const credentials = await this.repository.findAll(user.id);
+    console.log(credentials)
+    const credentialsDescrypt = credentials.map((credential)=>({
+      id: credential.id,
+      userName: credential.userName,
+      password: this.cryptr.decrypt(credential.password),
+      credentialTitle: credential.credentialTitle,
+      url: credential.url,
+      userId: credential.userId
+    }))
+    return credentialsDescrypt;
   }
 
   async findOne(id: number, user: Users) {
-    // const cryptr = new Cryptr("MyKey")
     const credential = await this.repository.findOne(id)
     if(!credential) throw new NotFoundException("Credential not found!")
     if(credential.userId !== user.id) throw new ForbiddenException("Forbidden");
 
-    // credential.password = cryptr.decrypt(credential.password)
+    credential.password = this.cryptr.decrypt(credential.password)
     
     return credential;
   }
 
   async remove(id: number, user: Users) {
-    const credential = await this.findOne(id, user)
+    await this.findOne(id, user)
     return await this.repository.remove(id);
   }
 }
